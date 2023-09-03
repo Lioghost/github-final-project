f8dbfaa0e5c551c5385f0d1dde2783b40935b289
Leonel Rodriguez Palacios <goliat282@gmail.com>
2023-09-02 22:48:33 -0600

    Lioghost
    Lioghost

--- simple-interest.sh
+++ simple-interest.sh
@@ -4,7 +4,7 @@
    # Do not use this in production. Sample purpose only.
    # Author: Upkar Lidder (IBM)
    # Additional Authors:
-   # <your GitHub username>
+   # Lioghost
    # Input:
    # p, principal amount
    # t, time period in years
